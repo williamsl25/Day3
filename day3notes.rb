7afc378c2f7c7381c24ee744d0ca12e53b2fa2d7
Lindsay Williams <williamsl25@yahoo.com>
2015-01-07 10:49:02 -0500

    this is a message

---
+++ day3notes.rb
@@ -0,0 +1,13 @@
+# git - every time you save, git is saving a snapshot in that point of time of whatever you commit (changed/saved/added)
+# everytime you make a change you can look at the commit to see what it was
+# if you add it will turn green, if you take away it will turn red
+
+# most important git commands 
+# git status- will tell you what changes you have made/status of the file
+# git add - you add files
+# git commit- will take the info that is ready to go to the repository
+# git checkout
+# git pull
+# git push origin master - 
+# git clue
+# *add, commit, push to get info on server
